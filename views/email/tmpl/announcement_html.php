9a0bc40d7fdc0aaf10b407c40de066ec2ec33ccd
M. Drew LaMar <mdlama@wm.edu>
2024-10-21 08:43:45 -0400

    [feat] Accessibility changes to email templates
    Added override of email.php
    Modified the following emails:
     - Group announcements
     - Group forum posts
    
    (cherry picked from commit 725db8ef4565400fa76fba2a949ca43ebcbd32ca)

--- views/email/tmpl/announcement_html.php
+++ views/email/tmpl/announcement_html.php
@@ -27,18 +27,14 @@ if ($this->announcement->priority == 1)
 	<table class="tbl-header" width="100%" cellpadding="0" cellspacing="0" border="0">
 		<tbody>
 			<tr>
-				<td width="10%" align="left" valign="bottom" nowrap="nowrap" class="sitename">
-					<?php echo Config::get('sitename'); ?>
+				<td width="1%" align="left" valign="middle">
+					<img src="https://qubeshub.org/app/site/media/images/emails/bullhorn-solid.png" width="24" height="24" style="border:none;" />
 				</td>
-				<td width="80%" align="left" valign="bottom" class="tagline mobilehide">
-					<span class="home">
-						<a href="<?php echo Request::base(); ?>" style="color: #666; font-weight: bold; text-decoration: none; border: none;"><?php echo Request::base(); ?></a>
-					</span>
-					<br />
-					<span class="description"><?php echo Config::get('MetaDesc'); ?></span>
+				<td width="9%" align="right" valign="bottom" nowrap="nowrap" class="component left">
+					Announcement
 				</td>
-				<td width="10%" align="right" valign="bottom" nowrap="nowrap" class="component">
-					Group Announcement
+				<td width="90%" align="right" valign="bottom" class="sitename group">
+					<?php echo $group->get('description'); ?>
 				</td>
 			</tr>
 		</tbody>
@@ -55,50 +51,58 @@ if ($this->announcement->priority == 1)
 	</table>
 	<!-- End Spacer -->
 
-	<table id="ticket-info" width="100%" cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse; border: 1px solid <?php echo $bdcolor; ?>; background: <?php echo $bgcolor; ?>; font-size: 0.9em; line-height: 1.6em; background-image: -webkit-gradient(linear, 0 0, 100% 100%,
-										color-stop(.25, rgba(255, 255, 255, .075)), color-stop(.25, transparent),
-										color-stop(.5, transparent), color-stop(.5, rgba(255, 255, 255, .075)),
-										color-stop(.75, rgba(255, 255, 255, .075)), color-stop(.75, transparent),
-										to(transparent));
-	background-image: -webkit-linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
-									transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
-									transparent 75%, transparent);
-	background-image: -moz-linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
-									transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
-									transparent 75%, transparent);
-	background-image: -ms-linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
-									transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
-									transparent 75%, transparent);
-	background-image: -o-linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
-									transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
-									transparent 75%, transparent);
-	background-image: linear-gradient(-45deg, rgba(255, 255, 255, .075) 25%, transparent 25%,
-									transparent 50%, rgba(255, 255, 255, .075) 50%, rgba(255, 255, 255, .075) 75%,
-									transparent 75%, transparent);
-									-webkit-background-size: 30px 30px;
-									-moz-background-size: 30px 30px;
-									background-size: 30px 30px;">
-		<thead class="mobilehide">
-			<tr>
-				<th style="font-weight: normal; border-bottom: 1px solid <?php echo $bdcolor; ?>; padding: 8px; text-align: left" align="left">
-					<strong><?php echo $group->get('description'); ?></strong>,
-					<a href="<?php echo $groupLink; ?>"><?php echo $groupLink; ?></a>
-				</th>
-			</tr>
-		</thead>
+	<table id="ticket-info" width="100%" cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse; border: 1px solid <?php echo $bdcolor; ?>; background: <?php echo $bgcolor; ?>; font-size: 0.9em; line-height: 1.6em;">
 		<tbody>
 			<tr>
 				<td width="100%" style="padding: 8px;">
-					<table style="border-collapse: collapse;" cellpadding="0" cellspacing="0" border="0">
+					<table style="border-collapse: collapse; font-size: 1.1em;" cellpadding="0" cellspacing="0" border="0">
 						<tbody>
+							<?php if ($group->get('logo')) { ?>
 							<tr>
-								<td style="text-align: left; padding: 20px;" align="left">
-									<a href="<?php echo $groupLink . '/announcements'; ?>"><?php echo $groupLink . '/announcements'; ?></a>
+								<td valign="top" rowspan="3">
+									<img style="max-height: 75px; max-width: 100px; width: auto; height: auto;" src="<?php echo rtrim(Request::root(), '/') . '/' . ltrim($group->getLogo(), '/'); ?>" alt="<?php echo $this->escape($group->get('description')); ?>" />
 								</td>
 							</tr>
+							<?php } ?>
 							<tr>
-								<td style="text-align: left; padding: 20px;" align="left">
+								<th style="text-align: right; padding: 0 0.5em 0 0.75em; font-weight: bold; white-space: nowrap;" align="right">Group:</th>
+								<td style="text-align: left; padding: 0 0.5em;" align="left"><a href="<?php echo $groupLink; ?>"><?php echo $group->get('description'); ?></a></td>
+							</tr>
+							<tr>
+								<th style="text-align: right; padding: 0 0.5em 0 0.75em; font-weight: bold; white-space: nowrap;" align="right">Priority:</th>
+								<td style="text-align: left; padding: 0 0.5em;" align="left"><?php echo ($this->announcement->priority == 1 ? 'Elevated' : 'Normal'); ?></td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+
+	<table id="announcement" width="100%" cellpadding="0" cellspacing="0" border="0" style="border-collapse: collapse; line-height: 1.1em; margin: 1em 0 0 0">
+		<tbody>
+			<tr>
+				<td>
+					<table width="100%" style="border-collapse: collapse;" cellpadding="0" cellspacing="0" border="0">
+						<tbody>
+							<tr>
+								<td style="text-align: left; padding: 0 1em 0 1em;" align="left">
 									<?php echo $this->announcement->get('content'); ?>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<table width="100%" style="border-collapse: collapse; padding: 0 1em 0 1em;" cellpadding="0" cellspacing="0" border="0">
+						<tbody>
+							<tr>
+								<td>
+								</td>
+								<td style="text-align: right; color: #666; font-size: 0.8em; padding: 0 1em 0 1em;" align="right">
+									<a href="<?php echo $groupLink . '/announcements'; ?>">View this announcement on <?php echo Config::get('sitename'); ?></a>
 								</td>
 							</tr>
 						</tbody>
@@ -119,11 +123,12 @@ if ($this->announcement->priority == 1)
 	<!-- End Spacer -->
 
 	<!-- Start Footer -->
-	<table class="tbl-footer" width="100%" cellpadding="0" cellspacing="0" border="0">
+	<table class="tbl-footer group" width="100%" cellpadding="0" cellspacing="0" border="0">
 		<tbody>
 			<tr>
-				<td align="left" valign="bottom">
-					<span><?php echo Config::get('sitename'); ?> sent this email because you belong to the <a href="<?php echo $groupLink; ?>"><?php echo $group->get('description'); ?></a> group.
+				<td align="center" valign="bottom">
+					<span>You received this message because you are a member of the <a href="<?php echo $groupLink; ?>"><?php echo $group->get('description'); ?></a> group on <a href="<?php echo Request::base(); ?>"><?php echo Config::get('sitename'); ?></a>.</span><br />
+					<span>To unsubscribe from <a href="<?php echo $groupLink; ?>"><?php echo $group->get('description'); ?></a> announcements, you must <a href="<?php echo $groupLink . '/cancel'; ?>">cancel your group membership</a>.
 				</td>
 			</tr>
 		</tbody>
